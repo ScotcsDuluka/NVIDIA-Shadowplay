1ee6b3a67bc3d7c95e1ba0e800ba0fd04b6fc1a4
ScotcsDuluka <agkarath.truajnok@gmail.com>
2024-10-16 19:49:30 +0700

    src

--- obj/x64/Release/net8.0-windows10.0.26100.0/NVIDIA Share.AssemblyInfo.vb
+++ obj/x64/Release/net8.0-windows10.0.26100.0/NVIDIA Share.AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Reflection
  Assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright© by Scotcs Duluka"),  _
  Assembly: System.Reflection.AssemblyDescriptionAttribute("NVIDIA ShadowPlay™ for All Users"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("2.59.363"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.59.363+407bc4711a5ffd17ded730ea780dfcf86021f586"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.59.363+fb94a1cebbb18b5863994e9070f38a08c2709d3f"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("NVIDIA ShadowPlay"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("NVIDIA ShadowPlay"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("2.59.363"),  _
