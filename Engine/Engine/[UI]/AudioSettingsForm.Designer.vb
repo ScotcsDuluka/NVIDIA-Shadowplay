3b97f34c6d585bf709a2450fc8010915827ff8f2
ScotcsDuluka <akkaruch3005@gmail.com>
2026-08-16 18:00:10 +0700

    fix(ui): convert AudioSettingsForm from overlay to normal dialog
    AudioSettingsForm was designed as an overlay (like ShadowPlay overlay):
      - WS_EX_TOOLWINDOW (hidden from Alt-Tab/taskbar)
      - WM_NCHITTEST + HTTRANSPARENT (click-through on empty areas)
      - OPEN_UI timer polling 'Audio.UI' file to show/hide
      - Opacity=0 when hidden
    
    This prevented normal .Show()/.Hide()/.ShowDialog() usage.
    
    Removed:
      - All DllImport (SetWindowLong/GetWindowLong)
      - WndProc override (click-through)
      - HideFromAltTab() method
      - OPEN_UI timer (both .vb and .Designer.vb)
      - Audio.UI file polling logic
    
    Now it's a standard WinForms dialog:
      - BT_Back_Click → Me.Close()
      - btnCancel_Click → Me.Close()
      - Can use .ShowDialog() from UI_Engine

--- Engine/Engine/[UI]/AudioSettingsForm.Designer.vb
+++ Engine/Engine/[UI]/AudioSettingsForm.Designer.vb
@@ -37,7 +37,6 @@ Partial Class AudioSettingsForm
         PictureBox16 = New PictureBox()
         hg2 = New PictureBox()
         Audio_Capture_Menutext = New Label()
-        OPEN_UI = New System.Windows.Forms.Timer(components)
         grpTrackMode.SuspendLayout()
         grpSystem.SuspendLayout()
         CType(trkSystemVol, ComponentModel.ISupportInitialize).BeginInit()
@@ -380,11 +379,6 @@ Partial Class AudioSettingsForm
         Audio_Capture_Menutext.TabIndex = 0
         Audio_Capture_Menutext.Text = "Audio Capture"
         ' 
-        ' OPEN_UI
-        ' 
-        OPEN_UI.Enabled = True
-        OPEN_UI.Interval = 1
-        ' 
         ' AudioSettingsForm
         ' 
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
@@ -454,6 +448,5 @@ Partial Class AudioSettingsForm
     Friend WithEvents PictureBox16 As PictureBox
     Friend WithEvents hg2 As PictureBox
     Friend WithEvents Audio_Capture_Menutext As Label
-    Friend WithEvents OPEN_UI As System.Windows.Forms.Timer
     Friend WithEvents Label2 As Label
 End Class
